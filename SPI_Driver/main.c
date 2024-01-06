99a5c1c617a8c02d4bd7378b134136b2b1de93a9
pi2712 <quanvm198@gmail.com>
2024-01-06 17:12:36 +0700

    MAX7219 Init done with self-written library

--- SPI_Driver/main.c
+++ SPI_Driver/main.c
@@ -6,11 +6,43 @@
 #define CS_Set()		GPIO_Reset(GPIO_A, 4)
 #define CS_Reset()	GPIO_Set(GPIO_A, 4)
 
+#define DIGIT_0_ONLY		0
+#define DIGIT_0_TO_1		1
+#define DIGIT_0_TO_2		2
+#define DIGIT_0_TO_3		3
+#define DIGIT_0_TO_4		4
+#define DIGIT_0_TO_5		5
+#define DIGIT_0_TO_6		6
+#define DIGIT_0_TO_7		7
+
 
 void SPI_SendData(uint8_t* data){
 	CS_Set();
 	SPI_Transmit(SPI1, data, 1);
 	CS_Reset();
+}
+
+void MAX7219_SendData(uint8_t address, uint8_t data){
+	SPI_SendData(&address);
+	SPI_SendData(&data);
+}
+
+void MAX7219_PowerOn(){
+	MAX7219_SendData(0x0C, 0x01);
+}
+
+void MAX7219_SetIntensity(uint8_t Intensity){
+		MAX7219_SendData(0x0A, Intensity);
+}
+
+void MAX7219_SetScanLimit(uint8_t ScanLimit){
+	MAX7219_SendData(0x0B, ScanLimit);
+}
+
+void MAX7219_Init(uint8_t Intensity, uint8_t ScanLimit){
+	MAX7219_PowerOn();
+	MAX7219_SetIntensity(Intensity);
+	MAX7219_SetScanLimit(ScanLimit);
 }
 
 int main(void){
@@ -20,17 +52,13 @@ int main(void){
 	SPI_IO_Init();
 	SPI_Init(SPI1, SPI_Master);
 
-	
+	MAX7219_Init(10, DIGIT_0_TO_3);
 	/*
 	GPIO_Reset(GPIO_A, 4);
 	SPI_Transmit(SPI1, &data, 1);
 	GPIO_Set(GPIO_A, 4);
 	*/
-	uint8_t data = 0x21;
-	SPI_SendData(&data);
 	
-	data = 0x22;
-	SPI_SendData(&data);
 	
 	while(1){
 		
